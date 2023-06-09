9d213d9791d7e4b6167db14c0df242f521a81d49
HyeseungNA <nahs00122@gmail.com>
2023-06-09 23:38:56 +0900

    알고리즘 문제 추가

--- programmers/6.py
+++ programmers/6.py
@@ -1,42 +1,26 @@
 def solution(tickets):
-    def dfs(name):
-        nonlocal answer,tmp
-        tmp = {}
-        for depart in range(len(tickets)):
-            for arrive in range(len(tickets)):
-                # 같은 출발지이고, 방문을 안하고, 다음 경로로 이동할 수 있을때 
-                if tickets[depart][0] == name and visited[depart][1] == 0 and tickets[depart][1] == tickets[arrive][0] and visited[arrive][1] == 0:
-                    # 딕셔너리에 넣는과정
-                    tmp[depart] = tickets[depart][1]
-        print(tmp)
-        # tmp 정렬해서 맨 처음값 뽑아내기
-        if len(tmp) >= 1:
-            dic = sorted(tmp.items(),key = lambda x:x[1])
-            # 방문처리하고
-            next_y = dic[0][0]
-            next_name = dic[0][1]
-            visited[next_y][1] = 1
-
-            # 결과값에 공항 이름 넣고
-            answer.append(next_name)
-            print(next_name)
-            #dfs돌아
-            dfs(next_name)
-
-              
-    answer = ['ICN']
-    tmp = []
-    visited = [[0]* 2 for _ in range(len(tickets))]
-    # 인천에서 출발!
-    for i in range(len(tickets)):
-        if tickets[i][0] == 'ICN':
-            visited[i][0] = 1
-            dfs(tickets[i][0])
-            break
+    
+    answer = []
+    visited = [0] * len(tickets)
+    
+    def dfs(airport, path):
+        # 티켓 다 쓰면
+        if len(path) == len(tickets) + 1:
+            answer.append(path)
+            return
         
-    # 남은 공항 추가
-    for i in range(len(tickets)):
-        if visited[i][1] == 0:
-            answer.append(tickets[i][1])
-
-    return answer
+        for idx, ticket in enumerate(tickets):
+            # 방문 안하고 도착지가 다음 출발지랑 같을 때
+            if visited[idx] == 0 and airport == ticket[0]:
+                visited[idx] = 1
+                dfs(ticket[1], path + [ticket[1]])
+                visited[idx] = 0
+    
+    dfs('ICN',["ICN"])
+    
+    answer.sort()
+    return answer[0]
+        
+        
+        
+        
