071584df574e632c7473aed938db5d62c765be36
HyeseungNA <nahs00122@gmail.com>
2023-07-26 12:16:28 +0900

    그리디 bfs로 구현

--- baekjoon/16953.py
+++ baekjoon/16953.py
@@ -1,36 +1,16 @@
-start, end = map(int,input().split())
 from collections import deque
-lst = []
-cnt = 0
-
-
-def bfs():
-    while q:
-        print(q)
-        start,ne,cnt  = q.popleft()
-        if start == end:
-            lst.append(cnt)
-        elif start > end:
-            continue
-
-        start = ne
-        n_1 = (ne * 2)
-        n_2 = (ne * 10) + 1
-
-        q.append((start,n_1,cnt+1))
-        q.append((start,n_2,cnt+1))
-
+a,b = map(int,input().split())
 q = deque()
-n_1 = start * 2
-n_2 = (start * 10 ) + 1
-cnt = 0
-q.append((start,n_1,cnt))
-q.append((start,n_2,cnt))
+q.append((a,1))
 
-bfs()
-
-if len(lst) == 0:
-    print(-1)
-
+while(q):
+    n,t = q.popleft()
+    if n > b:
+        continue
+    if n == b:
+        print(t)
+        break
+    q.append(((n* 10)+1,t+1))
+    q.append((n*2,t+1))
 else:
-    print(min(lst))
+    print(-1)
