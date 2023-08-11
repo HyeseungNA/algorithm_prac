36a1029600fc254bf7bfd8c51293ce94ff65f157
HyeseungNA <nahs00122@gmail.com>
2023-08-11 09:34:34 +0900

    최소공배수 오답정리

--- programmers/12953.py
+++ programmers/12953.py
@@ -1,17 +1,9 @@
+from fractions import gcd
+
 def solution(arr):
-    # 최소 구하기
-    Min = max(arr)
-    while True:
-        cnt = 0
 
-        for num in arr:
-            if Min % num == 0:
-                cnt += 1
-            else:
-                Min += 1
-        if cnt == len(arr):
-            break
+    answer = arr[0]
 
-
-
-    return Min
+    for num in arr:
+        answer = (num * answer) // gcd(answer,num)
+    return answer
