18c41b9da60cb364d8a410454924831776d73e4b
HyeseungNA <nahs00122@gmail.com>
2023-08-14 09:28:42 +0900

    딕셔너리 재풀이

--- programmers/138276.py
+++ programmers/138276.py
@@ -1,22 +1,23 @@
-def solution(k, tangerine):
-    dic = dict()
-
-    # dic 안에 다 넣기
-    for i in range(len(tangerine)):
-        if tangerine[i] in dic:
-            dic[tangerine[i]] += 1
-        else:
-            dic[tangerine[i]] = 1
-    
-    sorted_dic = dict(sorted(dic.items(),key=lambda item:item[1],reverse=True))
+def solution(k,tangerine):
+    # 카운트
     answer = 0
-    # 딕셔너리를 돌면서 total 비교하기 
-    for key in sorted_dic:
-        k -= sorted_dic[key]
-        answer += 1
+    # 딕셔너리 만들고 요소들 넣기
+    dic = dict()
+    for i in range(len(tangerine)):
+        if tangerine[i] not in dic:
+            dic[tangerine[i]] = 1
+        else:
+            dic[tangerine[i]] += 1
+        
+    # 내림차순 정렬하기
+    re_dict = dict(sorted(dic.items(),key=lambda x:x[1],reverse=True))
+    # print(re_dict)
 
+    for value in re_dict.values():
+        # 현재 값에서 빼주기
+        k -= value
+        answer += 1
+        # 현재 값이 0보다 작으면 멈추기
         if k <= 0:
             break
-
     return answer
-solution(6,[1, 3, 2, 5, 4, 5, 2, 3])
