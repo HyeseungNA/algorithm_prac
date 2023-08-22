1d048bc167caa0047eca149c4b744fbfa138dc33
HyeseungNA <nahs00122@gmail.com>
2023-08-22 15:14:17 +0900

    딕셔너리조합

--- programmers/42578.py
+++ programmers/42578.py
@@ -1,16 +1,14 @@
 def solution(clothes):
+    # 아이템을 넣을 딕셔너리
+    item = {}
     answer = 1
-    dic = dict()
-    # 딕셔너리에 넣고
-    # 딕셔너리에 넣기
     for c in clothes:
-        if c[1] not in dic:
-            dic[c[1]] = 1
+        if c[1] in item:
+            item[c[1]] += 1
         else:
-            dic[c[1]] += 1
-    
-    for value in dic.values():
-        answer *= (value + 1)
-    
+            item[c[1]] = 1
+    for value in item.values():
+        answer *= (value  + 1)
+    print(answer)
     return answer - 1
 solution([["yellow_hat", "headgear"], ["blue_sunglasses", "eyewear"], ["green_turban", "headgear"]])
