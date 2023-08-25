98e870a0aaa024cf9fcd7bdd29d014da0140460b
HyeseungNA <nahs00122@gmail.com>
2023-08-25 09:25:36 +0900

    dp 재풀이

--- baekjoon/1463.py
+++ baekjoon/1463.py
@@ -1,14 +1,12 @@
 n = int(input())
-d = [0] * 1000001
+dp = [0] * 1000001
 
 for i in range(2,n+1):
-
-    d[i] = d[i-1] + 1
+    dp[i] = dp[i-1] + 1
 
     if i % 2 == 0:
-        d[i] = min(d[i],d[i//2] + 1)
+        dp[i] = min(dp[i], dp[i//2] + 1)
     
     if i % 3 == 0:
-        d[i] = min(d[i] , d[i//3] + 1)
-
-print(d[n])
+        dp[i] = min(dp[i], dp[i//3] + 1)
+print(dp[n])
