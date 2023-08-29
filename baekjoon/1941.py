572cc34704e667c52ed81ec84f8f5196c03601d6
HyeseungNA <nahs00122@gmail.com>
2023-08-29 15:03:50 +0900

    dfs bfs

--- baekjoon/1941.py
+++ baekjoon/1941.py
@@ -3,32 +3,33 @@ def dfs(y,x,cnt):
     
     # cnt 가 7이면 돌아가기
     if cnt == 7:
+        print(visited)
+        print(answer)
         # 이다솜파 사람들이 4명이상이면 더해주기
         if answer.count('S') >= 4:
-            print(visited)
-            print(answer)
+            # print(visited)
+            # print(answer)
+            print('888888888888888888')
             total += 1
         return
     
-    for ny in range(5):
-        for nx in range(5):
-            # 방문을 확인하고
-            if visited[ny][nx] == 0:
-                # 가로 세로에 인접해 있는지 확인
-                dy = [-1,0]
-                dx = [0,-1]
+    # 가로, 세로 인접 확인
+    dy = [-1, 0, 1, 0]
+    dx = [0, -1, 0, 1]
 
-                for m in range(2):
-                    py = ny + dy[m]
-                    px = nx + dx[m]
+    for i in range(4):
+        ny = y + dy[i]
+        nx = x + dx[i]
 
-                    if visited[py][px] == 0:
-                        answer.append(girls[ny][nx])
-                        visited[ny][nx] = 1
-                        dfs(ny,nx, cnt + 1)
-                        answer.pop()
-                        visited[ny][nx] = 0
-
+        # 범위 확인, 방문 확인
+        if 0 <= ny < 5 and 0 <= nx < 5 and visited[ny][nx] == 0:
+            visited[ny][nx] = 1
+            answer.append(girls[ny][nx])
+            dfs(ny,nx,cnt + 1)
+            visited[ny][nx] = 0
+            answer.pop()
+                
+                    
 
 girls = [list(input()) for _ in range(5)]
 answer = [] # 칠공주 넣을 리스트
@@ -36,10 +37,12 @@ visited = [[0] * 5 for _ in range(5)]
 total = 0
 for i in range(5):
     for j in range(5):
-        # 방문 안했으면 하기
+        # 방문 안했으면 dfs 들어가기
         if visited[i][j] == 0:
             answer.append(girls[i][j])
             visited[i][j] = 1
             dfs(i,j,1) # x좌표, y좌표 , 깊이
+            if i==1 and j==0:
+            print('----------------')
             answer.pop()
-            visited[i][j]  = 0
+print(total)
