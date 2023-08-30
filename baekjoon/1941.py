95ef592775d7d8e16efe9acb5d05552ba3a26d41
HyeseungNA <nahs00122@gmail.com>
2023-08-30 21:46:46 +0900

    소문난 칠공주 재풀이

--- baekjoon/1941.py
+++ baekjoon/1941.py
@@ -1,48 +1,74 @@
-def dfs(y,x,cnt):
-    global total
-    
-    # cnt 가 7이면 돌아가기
+# 7개를 찾다
+# 다솜이 우위에 있는지
+# 인접하는지
+
+from collections import deque
+import copy
+def check():
+    q = deque()
+    v2 = copy.deepcopy(v1)
+
+    dy = [-1,0,1,0]
+    dx = [0,-1,0,1]
+
+    while q:
+        y,x = q.popleft()
+
+        for m in range(4):
+            ny = y + dy[i]
+            nx = x + dx[i]
+
+            if 0 <= nx < 5 and 0 <= ny < 5 and v2[ny][nx] == 0:
+                v2[ny][nx] = 1
+                q.append((ny,nx,cnt+1))
+    # 방문 안한것을 q안에 넣기
+    for i in range(5):
+        for j in range(5):
+            if v2[i][j] == 0:
+                v2[i][j] = 1
+                q.append((i,j,cnt))
+
     if cnt == 7:
-        print(visited)
-        print(answer)
-        # 이다솜파 사람들이 4명이상이면 더해주기
-        if answer.count('S') >= 4:
-            # print(visited)
-            # print(answer)
-            print('888888888888888888')
-            total += 1
-        return
+        return True
+    return
     
-    # 가로, 세로 인접 확인
-    dy = [-1, 0, 1, 0]
-    dx = [0, -1, 0, 1]
 
-    for i in range(4):
-        ny = y + dy[i]
-        nx = x + dx[i]
 
-        # 범위 확인, 방문 확인
-        if 0 <= ny < 5 and 0 <= nx < 5 and visited[ny][nx] == 0:
-            visited[ny][nx] = 1
-            answer.append(girls[ny][nx])
-            dfs(ny,nx,cnt + 1)
-            visited[ny][nx] = 0
-            answer.pop()
-                
-                    
+def dfs(lst,cnt):
 
-girls = [list(input()) for _ in range(5)]
-answer = [] # 칠공주 넣을 리스트
-visited = [[0] * 5 for _ in range(5)]
+    # cnt가 7이면 return 
+    if cnt == 7:
+        # 다솜이 우위에 있는 판별
+        if lst.count('S') >= 4:
+            if check():
+                total += 1
+            print(lst)
+        return
+
+
+    for i in range(5):
+        for j in range(5):
+            if v1[i][j] == 0:
+                lst.append(girls[i][j])
+                v1[i][j] = 1
+                dfs(lst,cnt + 1)
+                lst.pop()
+                v1[i][j] = 0
+                dfs(lst,cnt)
+
+
+
+girls = list(input() for _ in range(5))
+v1 = [[0] * 5 for _ in range(5)]
 total = 0
+# 7개를 찾다
 for i in range(5):
     for j in range(5):
-        # 방문 안했으면 dfs 들어가기
-        if visited[i][j] == 0:
-            answer.append(girls[i][j])
-            visited[i][j] = 1
-            dfs(i,j,1) # x좌표, y좌표 , 깊이
-            if i==1 and j==0:
-            print('----------------')
-            answer.pop()
+        lst = [] # 칠공주 넣을 곳
+        # 방문 안하면 dfs
+        if v1[i][j] == 0:
+            v1[i][j] = 1
+            lst.append(girls[i][j])
+            dfs(lst,1)
+
 print(total)
