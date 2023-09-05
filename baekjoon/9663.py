c30addebf01738e6c4fd24b4ece2d992d4eeea02
HyeseungNA <nahs00122@gmail.com>
2023-09-05 10:37:31 +0900

    n퀸 문제 해결

--- baekjoon/9663.py
+++ baekjoon/9663.py
@@ -1,32 +1,29 @@
 # 세로 시작
 # 가로 인접 확인 & 대각선 확인
 
-def dfs(y,x):
+def dfs(y):
+    global cnt
     if y == n:
         cnt += 1
         return
 
     for i in range(n):
-        # 가로 확인 및 대각선 확인
-        if v1[i] == 0:
-            # 대각선 확인에서 틀림
-            if abs((y+1)-y) != abs(i-x):
-                v1[i] = 1
-                v2[y+1][i] = 1
-                dfs(y+1,)
-
+        # 가로 확인, 우상향 확인, 좌상향 확인
+        if v1[i] == 0 and v2[y + i] == 0 and v3[y - i] == 0:
+            v1[i] = v2[y+i] = v3[y - i] = 1
+            dfs(y+1)
+            v1[i] = v2[y+i] = v3[y - i] = 0
         
-        return
 
 n = int(input())
-v1 = [0] * n
-v2 = [[0] * n for _ in range(n)]
+# 가로확인
+v1 = [0] * (2 * n)
+# 우상향 확인
+v2 = [0] * (2 * n)
+# 좌상향 확인
+v3 = [0] * (2 * n)
+
+# 총 개수
 cnt = 0
-for i in range(n):
-    # 방문처리해주기
-    v2[0][i] = 1
-    # 가로 방문 처리
-    v1[i] = 1
-    dfs(0,i) # 세로 시작, 가로 인덱스
-    v2[0][i] = 0
-    v1[i] = 0
+dfs(0)
+print(cnt)
