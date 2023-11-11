96c30bb4c282ded973cf19a969656073cd0fea7c
HyeseungNA <109335118+HyeseungNA@users.noreply.github.com>
2023-11-11 14:47:44 +0900

    [Silver III] Title: 타노스, Time: 44 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Silver/20310. 타노스/타노스.py
@@ -0,0 +1,14 @@
+n = list(input())
+one = n.count('1') // 2
+zero = n.count('0') // 2
+
+
+for _ in range(zero):
+    n = n[::-1]
+    n.remove('0')
+    n  = n[::-1]
+
+for _ in range(one):
+    n.remove('1')
+
+print(''.join(n))
