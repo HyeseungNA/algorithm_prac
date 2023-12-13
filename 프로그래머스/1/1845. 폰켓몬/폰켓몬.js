7f96ea436d8f9ebe128f1333fbc48c2b6741f07a
HyeseungNA <109335118+HyeseungNA@users.noreply.github.com>
2023-12-13 10:40:59 +0900

    [level 1] Title: 폰켓몬, Time: 0.25 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,19 +1,8 @@
 function solution(nums) {
     len = Math.floor(nums.length / 2)
-    const mons = {};
-    nums.forEach((el) => {
-        if (!mons[el]) {
-            mons[el] = 1
-        } else {
-            mons[el]  += 1
-        }
-    })
-    mons_len = Object.keys(mons).length
+    mons = new Set([...nums])
+    size = mons.size
     
-    if (mons_len >= len) {
-        return len
-    }else {
-        return mons_len
-    }
-    
+    return size >= len ?len : size
+     
 }
