b89245854ba959270253bf6a04c0b6f3964e320e
HyeseungNA <109335118+HyeseungNA@users.noreply.github.com>
2023-12-30 18:12:36 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 2.62 ms, Memory: 10.8 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
@@ -4,17 +4,17 @@ def solution(files):
         head, number, tail = '', '', ''
 
         number_check = False
-        for i in range(len(f)): # 문자열 자르기
-            if f[i].isdigit():  # 처음 나오는 숫자부터는 NUMBER로
+        for i in range(len(f)):
+            if f[i].isdigit():
                 number += f[i]
                 number_check = True
-            elif not number_check:  # NUMBER가 나오기 전까지는 HEAD
+            elif not number_check:
                 head += f[i]
-            else:               # NUMBER가 이미 나왔고, 숫자가 아닌 문자가 나오면 TAIL
+            else:
                 tail = f[i:]
                 break
-        answer.append((head, number, tail))  # HEAD, NUMBER, TAIL 하나의 튜플로 저장
+        answer.append((head, number, tail))
 
-    answer.sort(key=lambda x: (x[0].upper(), int(x[1])))  # HEAD 우선, NUMBER 차선으로 정렬
-
-    return [''.join(t) for t in answer]   # 원래 형태로 문자열 만들어서 반환
+    answer.sort(key=lambda x: (x[0].lower(), int(x[1])))
+    
+    return [''.join(t) for t in answer]
