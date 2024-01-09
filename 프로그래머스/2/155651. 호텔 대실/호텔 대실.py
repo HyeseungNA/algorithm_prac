3149dddd683c72b197dbdf88a9eab6e79621556c
HyeseungNA <109335118+HyeseungNA@users.noreply.github.com>
2024-01-09 11:02:16 +0900

    [level 2] Title: 호텔 대실, Time: 1.27 ms, Memory: 10.8 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.py
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.py
@@ -1,25 +1,3 @@
-from heapq import heappop, heappush
 def solution(book_time):
     answer = 0
-    times = []
-    for st,ed in book_time:
-        times.append([int(st[:2]) * 60 + int(st[3:]), int(ed[:2]) * 60 + int(ed[3:])])
-    
-    times.sort()
-    
-    heap = []
-    
-    for s,e in times:
-        if not heap:
-            heappush(heap,e)
-            answer += 1
-        else:
-            
-            if heap[0] <= s:
-                heappop(heap)
-                heappush(heap,e+10)
-            else:
-                answer += 1
-                heappush(heap,e+10)
-    
     return answer
