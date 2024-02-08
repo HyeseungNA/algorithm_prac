0aacb84c1afcd9a986deeb6b28f68aca8cc7371e
HyeseungNA <109335118+HyeseungNA@users.noreply.github.com>
2024-02-08 09:31:33 +0900

    [Gold V] Title: LCS, Time: 124 ms, Memory: 110536 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -1,15 +1,15 @@
-word1 = input()
-word2 = input()
+import sys
+read = sys.stdin.readline
 
-h = len(word1)
-w = len(word2)
-dp = [[0] * (w+1) for _ in range(h+1)]
+word1, word2 = read().strip(), read().strip()
+l1, l2 = len(word1), len(word2)
+cache = [0] * l2
 
-for i in range(1,h+1):
-    for j in range(1,w+1):
-        if word1[i-1] == word2[j-1]:
-            dp[i][j] = dp[i-1][j-1] + 1
-        else:
-            dp[i][j] = max(dp[i-1][j], dp[i][j-1])
-
-print(dp[h][w])
+for i in range(l1):
+    cnt = 0
+    for j in range(l2):
+        if cnt < cache[j]:
+            cnt = cache[j]
+        elif word1[i] == word2[j]:
+            cache[j] = cnt + 1
+print(max(cache))
