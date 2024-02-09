fa566cc181fd25a925b15ebed935b9a9880ddb26
HyeseungNA <109335118+HyeseungNA@users.noreply.github.com>
2024-02-09 15:13:01 +0900

    [Gold V] Title: 동전 1, Time: 144 ms, Memory: 111644 KB -BaekjoonHub

---
+++ 백준/Gold/2293. 동전 1/동전 1.py
@@ -0,0 +1,13 @@
+n, k = map(int, input().split())
+coin = []
+for _ in range(n):
+    coin.append(int(input()))
+
+dp = [0]* (k+1)
+dp[0] = 1
+for c in coin:
+    for i in range(1,k+1):
+        if i >= c:
+            dp[i] += dp[i-c]
+print(dp[k])
+
