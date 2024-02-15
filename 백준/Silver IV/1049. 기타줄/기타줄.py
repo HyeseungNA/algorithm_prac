b1ce3c4946d4806ba77bcb42bdf4840661b6a9b0
HyeseungNA <109335118+HyeseungNA@users.noreply.github.com>
2024-02-15 11:12:18 +0900

    [Silver IV] Title: 기타줄, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver IV/1049. 기타줄/기타줄.py
+++ 백준/Silver IV/1049. 기타줄/기타줄.py
@@ -1,33 +1,22 @@
-n,m = map(int,input().split())
-prices = []
+n,m = map(int,input().split(' '))
+
+s = list()
+o = list()
+
 for _ in range(m):
-    price = 0
-    prices.append(list(map(int,input().split())))
+    a,b = input().split(' ')
+    s.append(int(a))
+    o.append(int(b))
 
-packages = sorted(prices,key=lambda x:x[0])
-ones = sorted(prices, key=lambda x:x[1])
+s = min(s)
+o = min(o)
 
-packages_price = packages[0][0]
-ones_price = ones[0][1]
-packages_cnt = 0
-one_cnt = 0 
-
-if packages_price > ones_price * 6:
-    price = ones_price * n
-else:
-    if packages_price > (n % 6) * ones_price: # 나머지 비교 낱개가 더 쌀 때
-        packages_cnt = n // 6
-        one_cnt = n - (packages_cnt * 6)
-        price = packages_cnt * packages_price + one_cnt * ones_price
+if s < o * 6:
+    if s < (n % 6) * o:
+        print((n // 6) * s + s)
     else:
-        if n % 6 == 0:
-            packages_cnt = n // 6
-            price = packages_price * packages_cnt
-        else:
-            packages_cnt = (n // 6) + 1
-            price = packages_price * packages_cnt
-print(price)
-                
-            
+        print((n // 6) * s + (n % 6) * o)
 
- 
+elif s >= o * 6:
+    print(n * o)
+
