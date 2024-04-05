9e67fdc09dd0b62af6fb7331c64648a2d9511f66
HyeseungNA <109335118+HyeseungNA@users.noreply.github.com>
2024-04-05 09:18:05 +0900

    [Silver V] Title: 다리 놓기, Time: 148 ms, Memory: 109108 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.py
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.py
@@ -1,15 +1,11 @@
 t = int(input())
-dp = [[0] * 30 for _ in range(30)]
+dp = [[1] * (i+1) for i in range(31)]
 
-for i in range(30):
-    for j in range(30):
-        if j == 0 or j == i:
-            dp[i][j] = 1
-        elif j < i:
-            dp[i][j] = dp[i-1][j-1] + dp[i-1][j]
+for i in range(31):
+    for j in range(1,i):
+        dp[i][j] = dp[i-1][j-1] + dp[i-1][j]
+
 
 for _ in range(t):
     n,m = map(int,input().split())
     print(dp[m][n])
-
-    
