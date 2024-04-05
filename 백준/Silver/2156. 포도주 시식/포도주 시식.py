2be3c8766c63a084345c8a88552f264a30d97147
HyeseungNA <109335118+HyeseungNA@users.noreply.github.com>
2024-04-05 10:00:40 +0900

    [Silver I] Title: 포도주 시식, Time: 144 ms, Memory: 111712 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.py
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.py
@@ -1,20 +1,31 @@
 n = int(input())
-wine = []
+dp = [0] * (n+1)
+wines = [0]
+
 for _ in range(n):
-    wine.append(int(input()))
+    wines.append(int(input()))
 
+if n == 1:
+    dp[1] = wines[1]
+    print(dp[1])
 
-dp = [0] * n
-dp[0] = wine[0]
-if n > 1:
-    dp[1] = wine[0] + wine[1]
-    if n > 2:
-        dp[2] = max(dp[0] + wine[2], wine[1]+wine[2],dp[1])
+elif n == 2:
+    dp[1] = wines[1]
+    dp[2] = dp[1] + wines[2]
+    print(wines[1] + wines[2])
 
-        # 전전전 최대 + 전 와인 + 현재와인
-        # 전전 최대 + 현재 와인
-        # 전 최대
-        for i in range(3,n):
-            dp[i] = max(dp[i-3] + wine[i-1]+wine[i], dp[i-2]+wine[i],dp[i-1])
+elif n == 3:
+    dp[1] = wines[1]
+    dp[2] = dp[1] + wines[2]
+    dp[3] = max(dp[1] + wines[3] , dp[2], wines[2] + wines[3])
+    print(dp[3])
 
-print(max(dp))
+else:
+    dp[1] = wines[1]
+    dp[2] = dp[1] + wines[2]
+    dp[3] = max(dp[1] + wines[3] , dp[2], wines[2] + wines[3])
+
+    for i in range(4,n+1):
+        dp[i] = max(dp[i], dp[i-2] + wines[i], dp[i-1], dp[i-3] + wines[i-1] + wines[i])
+
+    print(max(dp))
