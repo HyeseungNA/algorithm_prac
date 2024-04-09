4c0ae1453ac6c124542dc61c5e2b4bf786e3eac6
HyeseungNA <109335118+HyeseungNA@users.noreply.github.com>
2024-04-09 09:34:02 +0900

    [Silver I] Title: 카드 구매하기, Time: 120 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
@@ -1,9 +1,10 @@
 n = int(input())
 cards = [0]
-cards = cards + list(map(int,input().split()))
+cards += list(map(int,input().split()))
 dp = [0] * (n+1)
-dp[1] = cards[1]
-for i in range(2,n+1):
-    for j in range(1,i+1):
-        dp[i] = max(cards[j] + dp[i-j], dp[i])
+
+for i in range(1,n+1):
+    for j in range(i+1):
+        dp[i] = max(dp[i], dp[j] + cards[i-j])
+
 print(dp[-1])
