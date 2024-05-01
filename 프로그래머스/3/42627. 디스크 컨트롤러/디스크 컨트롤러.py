dbd903d9c800705f8799fb1e4beb17f8436c2780
HyeseungNA <109335118+HyeseungNA@users.noreply.github.com>
2024-05-01 15:43:49 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -1,22 +1,24 @@
 def solution(jobs):
     answer = 0
-    now = 0
-    n = len(jobs)
-    jobs.sort(key=lambda x: x[0])  # 요청 시간을 기준으로 작업을 정렬
-    
+    now = 0 # 작업 완료 시간
+    n = len(jobs) 
+    jobs.sort(key=lambda x:x[0])
     while jobs:
         Min = int(12e8)
         idx = -1
         for i in range(len(jobs)):
-            if jobs[i][0] <= now:  # 현재 시간 이전에 요청된 작업일 때
-                if jobs[i][1] < Min:  # 소요 시간이 가장 짧은 작업 선택
+            if jobs[i][0] <= now:
+                if jobs[i][1] < Min:
                     Min = jobs[i][1]
                     idx = i
-        if idx == -1:  # 현재 시간 이전에 요청된 작업이 없을 때
-            now = jobs[0][0]  # 다음 작업의 요청 시간으로 시간을 이동
+        if idx == -1:
+            now = jobs[0][0]
         else:
+            
             answer += now - jobs[idx][0] + Min
             now += Min
             jobs.pop(idx)
+        
+        
     
     return answer // n
