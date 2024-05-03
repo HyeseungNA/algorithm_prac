812dc86db1e89ee437808739bd57eb01710126ae
HyeseungNA <109335118+HyeseungNA@users.noreply.github.com>
2024-05-03 22:00:15 +0900

    [level 3] Title: 헤비 유저가 소유한 장소, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
+++ 프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
@@ -4,5 +4,6 @@ FROM PLACES
 WHERE HOST_ID IN (SELECT HOST_ID
                  FROM PLACES
                  GROUP BY HOST_ID
-                 HAVING COUNT(*)>= 2)
-ORDER BY ID 
+                 HAVING COUNT(*) >= 2)
+
+ORDER BY ID
